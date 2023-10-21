de38881bf4bb578e2ddf2bfc05fd0b49623430c3
Leandro Pereira <opereira3591@gmail.com>
2023-10-21 16:54:16 +0100

    Barra de navegação atualizada

--- JS/loja.js
+++ JS/loja.js
@@ -1,18 +1,18 @@
-function comprar(event) {
+function comprar(event,id) {
   event.preventDefault();
-  const elemento_pai = document.getElementById("p_1");
+  const elemento_pai = document.getElementById(`p_${id}`);
   const preco = parseFloat(elemento_pai.querySelector(".preco").innerHTML);
   const quantidade = elemento_pai.querySelector(".quantidade").value;
   const titulo = elemento_pai.querySelector(".card-title").innerHTML;
   const total = (preco * quantidade).toFixed(2);
   const output = `Foram adquiridas ${quantidade} unidade(s) de "${titulo}" pelo valor total de ${total} Euros.`;
-  console.log(output);
   $("#precoJanela").modal("show");
-  document
-    .getElementById("precoJanela")
-    .querySelector(".texto-principal").innerHTML = output;
+  document.getElementById("precoJanela").querySelector(".texto-principal").innerHTML = output;
+  elemento_pai.querySelector(".quantidade").value = '1';
 }
 
+// Função para esconder o modal do preço do produto
 function fecharJanela() {
-    $("#precoJanela").modal("hide");
+  $("#precoJanela").modal("hide");
 }
+
